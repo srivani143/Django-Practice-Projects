335b92be3f840eba27bf3b6d0584ad3b995761ab
srivani143 <polukasanthosh573@gmail.com>
2023-11-07 10:47:19 -0800

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def drArjunReddy(request):
+    return HttpResponse("<h1><marquee>Hi Mama Come to my home i ordered a RUSSIAN let's Enjoy</marquee></h1>")
